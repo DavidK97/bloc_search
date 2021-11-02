6b00bde5e2a161a1e09eb0508d51556c3d192ae2
DavidK97 <klotzd.dev@gmail.com>
2021-11-02 21:42:42 +0100

    finished search test

---
+++ common_github_search/lib/bloc/github_search_bloc.dart
@@ -0,0 +1,44 @@
+import 'package:bloc/bloc.dart';
+import '../github_repository.dart';
+import '../search_result_error.dart';
+import '../search_result_item.dart';
+import 'package:equatable/equatable.dart';
+import 'package:stream_transform/stream_transform.dart';
+
+part 'github_search_event.dart';
+part 'github_search_state.dart';
+
+const _duration = const Duration(microseconds: 300);
+
+EventTransformer<Event> debounce<Event>(Duration duration) {
+  return (events, mapper) => events.debounce(duration).switchMap(mapper);
+}
+
+class GithubSearchBloc extends Bloc<GithubSearchEvent, GithubSearchState> {
+  GithubSearchBloc({required this.githubRepository})
+      : super(SearchStateEmpty()) {
+    on<TextChanged>(_onTextChanged, transformer: debounce(_duration));
+  }
+
+  final GithubRepository githubRepository;
+
+  void _onTextChanged(
+    TextChanged event,
+    Emitter<GithubSearchState> emit,
+  ) async {
+    final searchTerm = event.text;
+
+    if (searchTerm.isEmpty) return emit(SearchStateEmpty());
+
+    emit(SearchStateLoading());
+
+    try {
+      final results = await githubRepository.search(searchTerm);
+      emit(SearchStateSuccess(results.items));
+    } catch (error) {
+      emit(error is SearchResultError
+          ? SearchStateError(error.message)
+          : SearchStateError('something went wrong'));
+    }
+  }
+}
---
+++ flutter_github_search/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:common_github_search/bloc/github_search_bloc.dart';
+import 'package:common_github_search/github_cache.dart';
+import 'package:common_github_search/github_client.dart';
+import 'package:common_github_search/github_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_github_search/search_form.dart';
+
+void main() {
+  final GithubRepository githubRepository = GithubRepository(
+    GithubCache(),
+    GithubClient(),
+  );
+
+  runApp(App(
+    githubRepository: githubRepository,
+  ));
+}
+
+class App extends StatelessWidget {
+  const App({Key? key, required this.githubRepository}) : super(key: key);
+
+  final GithubRepository githubRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Github Search with bloc',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Github Search with bloc'),
+        ),
+        body: BlocProvider(
+          create: (_) => GithubSearchBloc(githubRepository: githubRepository),
+          child: SearchForm(),
+        ),
+      ),
+    );
+  }
+}
